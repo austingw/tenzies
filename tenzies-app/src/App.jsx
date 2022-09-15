b818be9130b98debbf136c0fe0219ddd3e840360
Austin Williams <senortinman@gmail.com>
2022-09-15 18:27:43 -0400

    Add Die component with hardcoded values, along with styling for all dice

--- tenzies-app/src/App.jsx
+++ tenzies-app/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Die from "./Die";
 
 function App() {
-  return <main>Test</main>;
+  return (
+    <main>
+      <div className="dice-container ">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ tenzies-app/src/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+  return (
+    <div className="die">
+      <h2 className="die-value">{props.value}</h2>
+    </div>
+  );
+}
