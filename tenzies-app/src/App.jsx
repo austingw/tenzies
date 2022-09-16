a3b8de811c3c575a37eb77acccb8567d5ea8e60c
Austin Williams <senortinman@gmail.com>
2022-09-15 22:24:48 -0400

    Add game title and roll button

--- tenzies-app/src/App.jsx
+++ tenzies-app/src/App.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Die from "./Die";
 
 function App() {
+  const [dice, setDice] = useState(getNewDice());
+
+  function getNewDice() {
+    const newDice = [
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+    ];
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(getNewDice());
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
     <main>
-      <div className="dice-container ">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <h2 className="game-title">Tenzies!</h2>
+      <div className="dice-container ">{diceElements}</div>
+      <button onClick={rollDice}>Roll</button>
     </main>
   );
 }
